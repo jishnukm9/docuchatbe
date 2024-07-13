bebe39b44a94c372e029c6f55ffaa1c62cd59e07
jishnukm9 <jishnukm2010@gmail.com>
2024-07-13 23:40:00 +0530

    added Procfile,runtime.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn docuchat.wsgi --log-file
--- docuchat/settings.py
+++ docuchat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-16hd4vzqlbdw3(!*a3p2)f6b47qz#s5n@1z33ydvz#!)9$vb%%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -128,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
